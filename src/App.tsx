5292dc501b73d8b7951b10df6da72f11e7f7b30f
luongphucdien <work.luongphucdien@gmail.com>
2023-09-09 19:14:33 +0700

    fix: Fix AR unsupported on mobile with HTTPS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { Text } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+import { ARButton, Controllers, XR } from "@react-three/xr"
 function App() {
     return (
         <>
-            <div className="">Test</div>
-            <p />
+            <ARButton />
+            <Canvas>
+                <XR referenceSpace="local">
+                    <ambientLight />
+                    <mesh>
+                        <Text
+                            anchorX="center"
+                            anchorY="middle"
+                            color="#000"
+                            fontSize={0.1}
+                            position={[0, 0, 0.06]}
+                        >
+                            Test
+                        </Text>
+                    </mesh>
+                    <pointLight position={[10, 10, 10]} />
+                    <Controllers />
+                </XR>
+            </Canvas>
         </>
     )
 }
