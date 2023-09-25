678d93ccda1b8664926fb13f9e5701c8f1b08a61
luongphucdien <work.luongphucdien@gmail.com>
2023-09-26 00:31:34 +0700

    feat: Finish Sign Up functionalities

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,4 +1,5 @@
-import axios from "axios"
+import axios, { HttpStatusCode } from "axios"
+import { Buffer } from "buffer"
 import { encrypt } from "./security"
 
 const _raw_API_URL: string = import.meta.env.VITE_API_URL
@@ -16,25 +17,40 @@ export const checkIfEmailExisted = async (
     isExistedCallback: () => void,
     isNotExistedCallback: () => void
 ) => {
-    return await axios
-        .get(`${API_URL}/api/users/${email}`)
-        .then((res) => {
-            if (res.data.fetchedEmails.length === 0) {
-                isNotExistedCallback()
-            } else isExistedCallback()
+    const base64Email = Buffer.from(email).toString("base64")
 
-            console.log(res.data)
+    return await axios
+        .get(`${API_URL}/api/users/${base64Email}`)
+        .then((res) => {
+            if (res.data.code === HttpStatusCode.NotFound)
+                isNotExistedCallback()
+            else if (res.data.code === HttpStatusCode.Ok) isExistedCallback()
         })
         .catch((err) => console.log(err))
 }
 
-export const signUp = async (email: string, password: string) => {
+export const signUp = async (
+    email: string,
+    password: string,
+    setSignUpState: (state: boolean) => void,
+    signUpDoneCallback: () => void
+) => {
     const _encryptedPW = encrypt(password)
+    const _base64Email = Buffer.from(email).toString("base64")
+
     return await axios
         .post(`${API_URL}/api/users`, {
             email: email,
+            base64Email: _base64Email,
             password: _encryptedPW,
         })
-        .then((res) => console.log(res.data))
-        .catch((err) => console.log(err))
+        .then((res) => {
+            if (res.data.code === HttpStatusCode.Ok) {
+                setSignUpState(true)
+                signUpDoneCallback()
+            } else setSignUpState(false)
+        })
+        .catch(() => {
+            setSignUpState(false)
+        })
 }
