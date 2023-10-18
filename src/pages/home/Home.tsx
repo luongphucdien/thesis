fee1c87c37a9182d89595d353cad31d5fce4ef24
luongphucdien <work.luongphucdien@gmail.com>
2023-10-19 01:53:39 +0700

    update: Generate floor from vertices

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,27 +1,8 @@
-import { useCookies } from "react-cookie"
 import { IconContext } from "react-icons"
 import { TbAugmentedReality } from "react-icons/tb"
-import { useNavigate } from "react-router-dom"
-import { Button, LinkButton } from "../../components/button"
+import { LinkButton } from "../../components/button"
 
 export const Home = () => {
-    // ---FOR MOBILE TEST ONLY, REMOVE FOR PRODUCTION BUILD--- //
-    const nav = useNavigate()
-    const [cookies, setCookies] = useCookies(["userToken", "userUID"])
-    // const userUID = Buffer.from("work.luongphucdien@gmail.com").toString(
-    //     "base64"
-    // )
-    const mobileTest = () => {
-        setCookies("userToken", "TEST---FOR MOBILE ONLY", {
-            path: "/",
-            maxAge: 86400,
-        })
-        // setCookies("userUID", userUID, { path: "/", maxAge: 86400 })
-        nav(0)
-    }
-    // ---FOR MOBILE TEST ONLY, REMOVE FOR PRODUCTION BUILD--- //
-    // ---END--- //
-
     return (
         <div className="flex h-full flex-col items-center justify-center text-neutral-100">
             <IconContext.Provider value={{ size: "250px" }}>
@@ -36,8 +17,6 @@ export const Home = () => {
                 <LinkButton to="/sign-in" variant="non opaque">
                     To Sign In
                 </LinkButton>
-
-                <Button onClick={mobileTest}>FOR MOBILE ONLY</Button>
             </div>
         </div>
     )
