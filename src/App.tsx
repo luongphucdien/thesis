b41c345a6e15fd0c61298bc1245318658ccff650
luongphucdien <work.luongphucdien@gmail.com>
2023-11-08 19:02:38 +0700

    update: Setup for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { CustomRoute } from "./routes"
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/">
             <CustomRoute />
         </BrowserRouter>
     )
